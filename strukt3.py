ae60dad619c0372faee77fb726bee3e2a235769f
DEV-COM <kwp@tlen.pl>
2023-12-05 12:02:09 +0100

    12:02 05.12

---
+++ strukt3.py
@@ -0,0 +1,6 @@
+slownik = {"A": 1}
+slownik["A"] = 100
+slownik["B"] = 1000
+slownik[123] = [1, 2, 3]
+print(slownik[123][1])
+print(slownik.keys())
---
+++ strukt4.py
@@ -0,0 +1,19 @@
+zestaw = {10, 20, 30, 40, 20, 40}
+zestaw.add(100)
+zestaw.add(100)
+print(zestaw)
+lista = list(zestaw)
+lista.sort()
+print(lista[0])
+
+a = 5.5
+b = 66
+c = int(a)
+d = float(b)
+print(c)
+print(d)
+
+
+x = input("Podaj liczbę:")
+suma = 10 + int(x)
+print(suma)
