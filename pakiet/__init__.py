942ecd8a0c40ec8dbe56eceaa084c65ad55eb0ba
DEV-COM <kwp@tlen.pl>
2023-12-05 14:22:24 +0100

    14:22 05.12

---
+++ pakiet/__init__.py
@@ -0,0 +1,7 @@
+customers = [
+    dict(id=1, total=200, code="f20"),
+    dict(id=2, total=1200, code="f40"),
+    dict(id=3, total=2200, code="p20"),
+    dict(id=4, total=3200, code="f10"),
+]
+
---
+++ start.py
@@ -0,0 +1,7 @@
+import pakiet
+from pakiet import *
+
+customers = (1, 2, 3)
+
+print(pakiet.customers[0]['code'])
+print(customers)
