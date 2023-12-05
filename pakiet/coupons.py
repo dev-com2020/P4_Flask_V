7c6405b4ce55add6920feda9130c41598cf27c8f
DEV-COM <kwp@tlen.pl>
2023-12-05 14:48:33 +0100

    14:48 05.12

--- pakiet/coupons.py
+++ pakiet/coupons.py
@@ -1,15 +1,21 @@
 from pakiet import customers
 
-print(customers)
 
-for customer in customers:
-    code = customer['code']
-    if code == 'f20':
-        customer['discount'] = 20.0
-    elif code == 'f40':
-        customer['discount'] = 15.0
-    else:
-        customer['discount'] = 0.0
+def drukuj_rabat():
+    print(customers)
 
-for customer in customers:
-    print(customer['id'], " | ", customer['total'], " | ", customer['discount'])
+    for customer in customers:
+        code = customer['code']
+        if code == 'f20':
+            customer['discount'] = 20.0
+        elif code == 'f40':
+            customer['discount'] = 15.0
+        else:
+            customer['discount'] = 0.0
+
+    for customer in customers:
+        print(customer['id'], " | ", customer['total'], " | ", customer['discount'])
+
+
+def calculate_vat(price, vat):
+    return price * (100 + vat) / 100
--- start.py
+++ start.py
@@ -1,9 +1,20 @@
 import pakiet
 from pakiet import *
-
+from pakiet import coupons
 
 customers = (1, 2, 3)
+wyniki_vat = []
 
 print(pakiet.customers[0]['code'])
 print(customers)
-
+coupons.drukuj_rabat()
+wynik23 = coupons.calculate_vat(1000, 23)
+wynik8 = coupons.calculate_vat(1000, 8)
+wynik0 = coupons.calculate_vat(1000, 0)
+print("Vat 23=", wynik23)
+print("Vat 8=", wynik8)
+print("Vat 0=", wynik0)
+wyniki_vat.append(wynik23)
+wyniki_vat.append(wynik8)
+wyniki_vat.append(wynik0)
+print(wyniki_vat)
