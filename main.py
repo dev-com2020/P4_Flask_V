0a367c95b05204850dc538415016793b0c139e30
DEV-COM <kwp@tlen.pl>
2023-12-05 10:42:37 +0100

    10:42 05.12

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
 print("Test")
+
+age = 42
+print(type(age))
+
+temp = 11.5
+print(type(temp))
+
+napis = "cokolwiek co mi przyjdzie do głowy &*&^$%*#&($^(*@ 432782484729"
+print(type(napis))
+print(napis.upper())
+# tutaj może być dowolna linia tekstu
+# tutaj następna linia komentarza...
+print(len(napis))
+# print(napis[62])
+'''
+tu jest komentarz
+tu jest komentarz
+tu jest komentarz
+tu jest komentarz
+'''
+czy_jest_zima = True
+czy_jest_lato = False
---
+++ strukt1.py
@@ -0,0 +1,18 @@
+# pustą listę
+osoby = []
+osoby.append("Tomek")
+osoby.append("ADAM")
+osoby.append("Agnieszka")
+osoby.append("Szymon")
+osoby.append("Dawid")
+print(osoby)
+print(osoby[0:3])
+print(osoby[3:])
+print(osoby[:2])
+print(osoby[-5:])
+print(len(osoby))
+osoby.pop()
+osoby.remove("Szymon")
+osoby.insert(1, "Michał")
+print(osoby)
+
