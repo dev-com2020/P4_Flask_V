0ed3fd2d4ca6e43ce210c25318bc29dcdafdf3dd
DEV-COM <kwp@tlen.pl>
2023-12-05 16:23:10 +0100

    16:23 05.12

--- wyjatki.py
+++ wyjatki.py
@@ -36,6 +36,12 @@ dane_popsute = [
     dict(name='Marcin', age='17'),
 ]
 
+dane_popsute2 = [
+    dict(name='Jan', age='age'),
+    dict(name='Dawid', age='19'),
+    dict(name='Marcin', age='17'),
+]
+
 
 def check_age(users, age):
     count = 0
@@ -45,8 +51,28 @@ def check_age(users, age):
                 count += 1
         except KeyError:
             print(f'Niepoprawne dane {user}')
+        except ValueError:
+            print(f"Niepoprawna wartość {user}")
     return count
 
 
-print(check_age(dane, 15))
-print(check_age(dane_popsute, 15))
+# print(check_age(dane, 15))
+# print(check_age(dane_popsute, 15))
+# print(check_age(dane_popsute2, 15))
+
+def check_age2(users, age):
+    count = 0
+    for user in users:
+        try:
+            user_age = int(user['age'])
+        except KeyError:
+            print(f'Niepoprawne dane {user}')
+        except ValueError:
+            print(f"Niepoprawna wartość {user}")
+        else:
+            count += 1 if user_age < age else 0
+        finally:
+            print(user)
+    return count
+
+print(check_age2(dane_popsute2, 15))
